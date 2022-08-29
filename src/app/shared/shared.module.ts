8beced679fb207782ad43aa22dad699a1cd89aec
Aman Patel <aman.patel@1rivet.local>
2022-08-29 15:38:29 +0530

    feat: db.json updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CheckValidtyDirective } from './Directives/check-validty.directive';
-import { CheckValidityDirective } from './check-validity.directive';
-import { CheckvalidityDirective } from './checkvalidity.directive';
-
 
 
 @NgModule({
   declarations: [
-    CheckValidtyDirective,
-    CheckValidityDirective,
-    CheckvalidityDirective
   ],
   imports: [
     CommonModule
