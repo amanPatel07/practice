3fff7e03c187f2101e0c16e13bf1a3f081d9629b
Aman Patel <aman.patel@1rivet.local>
2022-09-05 18:41:18 +0530

    fix: Table Scroll

--- src/app/employees/employee-list/employee-list-presentation/employee-list-presentation.component.ts
+++ src/app/employees/employee-list/employee-list-presentation/employee-list-presentation.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ScrollDispatcher } from '@angular/cdk/scrolling';
+import { Target } from '@angular/compiler';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Scroll } from '@angular/router';
 import { EmployeeListPresenterService } from '../employee-list-presenter/employee-list-presenter.service';
 
 @Component({
@@ -8,24 +11,23 @@ import { EmployeeListPresenterService } from '../employee-list-presenter/employe
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeeListPresentationComponent implements OnInit {
-
   /**
   * @name departmenrId Setter Getter for the departmentID
   * @description Get the departmenrID
   * @return The Department ID
   */
   @Input() public set departmentId(value: any) {
-    if (value) {
-      this._departmentId = value;     
-      this.getEmployeeListById();
-    }
-
+    // if (value) {
+    //   this._departmentId = value;
+    //   this.getEmployeeListById();
+    // }
+    value ?  this._departmentId = value : '';
+    this.getEmployeeListById();
   }
   public get departmentId(): any {
     return this._departmentId;
   }
   private _departmentId!: any;
-
   /**
    * @name EmployeeList Settter Getter for the Employeel List
    * @description Get the employee list by the department ID
@@ -34,15 +36,26 @@ export class EmployeeListPresentationComponent implements OnInit {
   @Input() public set employeeList(value: any | null) {
     if (value) {
       this._employeeList = value['employee'];
-      this.departmentName = value['departmentName']
+      this.departmentName = value['departmentName'];
     }
-    return
+    return;
   }
   public get employeeList(): any {
     return this._employeeList;
   }
   private _employeeList!: any;
-  public departmentName: any
+  public departmentName: any;
+
+  /**
+   * @name getThead 
+   * @description Element Reference
+   */
+  @ViewChild('getThead') getThead!: ElementRef<HTMLElement>;
+  /**
+   * @name getTbody
+   * @description Element Reference
+   */
+  @ViewChild('getTbody') getTbody!: ElementRef<HTMLElement>;
 
   /**
    * @name getEmployeeListById An event emitter
@@ -51,7 +64,7 @@ export class EmployeeListPresentationComponent implements OnInit {
   @Output() getEmployeeById: EventEmitter<any>;
 
   constructor(private cdr: ChangeDetectorRef) {
-    this.getEmployeeById = new EventEmitter()
+    this.getEmployeeById = new EventEmitter();
   }
 
   ngOnInit(): void {
@@ -63,7 +76,25 @@ export class EmployeeListPresentationComponent implements OnInit {
    * @description Get the empolyee list by the department ID.
    */
   private getEmployeeListById() {
-    this.getEmployeeById.emit(this._departmentId)
+    this.getEmployeeById.emit(this._departmentId);
   }
 
+  /**
+   * @name getTheadScroll
+   * @param event Get the Event of the element
+   * @description To scroll horizontally by ElementRef native element
+   */
+  public getTheadScroll(event: any) {
+    const getBody: any = this.getTbody.nativeElement;
+    getBody.scrollLeft = event.target.scrollLeft;
+  }
+  /**
+   * @name getTbodyScroll
+   * @param event Get the Event of the element
+   * @description To scroll horizontally by ElementRef native element
+   */
+  public getTbodyScroll(event: any) {
+    const getHead: any = this.getThead.nativeElement;
+    getHead.scrollLeft = event.target.scrollLeft;
+  }
 }
